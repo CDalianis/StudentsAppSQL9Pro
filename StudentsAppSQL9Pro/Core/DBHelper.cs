65895da4637694ec2e807c50f78614820fadcc8c
CDalianis <christos_dl@hotmail.com>
2026-04-26 14:00:53 +0300

    feat add db helper

--- StudentsAppSQL9Pro/Core/DBHelper.cs
+++ StudentsAppSQL9Pro/Core/DBHelper.cs
@@ -1,6 +1,20 @@
-﻿namespace StudentsAppSQL9Pro.Core
+﻿using Microsoft.Data.SqlClient;
+
+namespace StudentsAppSQL9Pro.Core
 {
     public class DBHelper
     {
+        private readonly string? _connectionString;
+
+        public DBHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+            // configuration["ConnectionStrings:DefaultConnection"]
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
